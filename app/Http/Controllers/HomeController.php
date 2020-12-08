267d57b8b1a793215714f901a60485dc47feb3ab
umar834 <63906422+umar834@users.noreply.github.com>
2020-12-08 20:53:38 +0500

    Register user error resolved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -27,12 +28,13 @@ class HomeController extends Controller
         $user = User::find(auth()->id());
         $role = $user->roles()->orderBy('name')->first();
         //get role of current user [$user->id]
-        $current_role = DB::table('role_user')->select(['role_id'])->where('user_id','=',$user->id)->first();
-        if($current_role->role_id == 1){
+        $current_role = DB::table('role_user')->select(['user_id'])->where('role_id','=',$user->id)->first();
+       
+        if($current_role->user_id == 1){
             return view('admin.index');
-        }else if($current_role->role_id == 2){
+        }else if($current_role->user_id == 2){
             return view('customer.index');
-        }else if($current_role->role_id == 3){
+        }else if($current_role->user_id == 3){
             return view('seller.index');
         }return view('home');
     }
